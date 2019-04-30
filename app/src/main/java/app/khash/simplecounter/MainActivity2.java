33fad1d3fba61d811a92c6a7e8e7b806652e130b
Khash <khash.021@gmail.com>
2019-05-01 02:56:08 +0430

    Modified layout and code to include bpm

--- app/src/main/java/app/khash/simplecounter/MainActivity2.java
+++ app/src/main/java/app/khash/simplecounter/MainActivity2.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
@@ -18,14 +19,20 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     final int MSG_STOP_TIMER = 1;
     final int MSG_UPDATE_TIMER = 2;
 
-    Stopwatch timer = new Stopwatch();
+    Stopwatch timer;
     final int REFRESH_RATE = 100;
 
-    Handler mHandler = new Handler()
-    {
+    final int BPM_DEFAULT = 60;
+
+    TextView textStopWatch, textCounter;
+    EditText textBpm;
+    Button buttonStart, buttonStop, buttonReset;
+
+    Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+
             switch (msg.what) {
                 case MSG_START_TIMER:
                     timer.start(); //start timer
@@ -33,24 +40,28 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                     break;
 
                 case MSG_UPDATE_TIMER:
-                    tvTextView.setText(""+ timer.getElapsedTimeSecs());
-                    Log.v(TAG, Long.toString(timer.getElapsedTimeSecs()));
-                    mHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER,REFRESH_RATE); //text view is updated every second,
+                    String input = timer.getBpmElapsed();
+                    if (input == null) {
+                        break;
+                    }
+                    String[] bpmElapsed = input.split(";");
+                    String counter = bpmElapsed[0];
+                    String elapsed = bpmElapsed[1];
+                    textStopWatch.setText(elapsed);
+                    textCounter.setText(counter);
+                    mHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER, REFRESH_RATE); //text view is updated every second,
                     break;                                  //though the timer is still running
                 case MSG_STOP_TIMER:
                     mHandler.removeMessages(MSG_UPDATE_TIMER); // no more updates.
                     timer.stop();//stop timer
-                    tvTextView.setText(""+ timer.getElapsedTimeSecs());
                     break;
 
                 default:
                     break;
             }
         }
-    };
+    };//handler
 
-    TextView tvTextView;
-    Button btnStart,btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,22 +69,35 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        tvTextView = findViewById(R.id.text_timer);
+        textStopWatch = findViewById(R.id.text_stop_watch);
+        textCounter = findViewById(R.id.text_counter);
+        textBpm = findViewById(R.id.text_bpm);
 
-        btnStart = findViewById(R.id.button_start);
-        btnStop= findViewById(R.id.button_stop);
-        btnStart.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
+        buttonStart = findViewById(R.id.button_start);
+        buttonStop = findViewById(R.id.button_stop);
+        buttonReset = findViewById(R.id.button_reset);
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonReset.setOnClickListener(this);
 
-    }
+    }//onCreate
 
     public void onClick(View v) {
-        if(btnStart == v)
-        {
+        if (v == buttonStart) {
+            if (textBpm.getText().toString().trim().length() < 1) {
+                textBpm.setText(Integer.toString(BPM_DEFAULT));
+                timer = new Stopwatch(BPM_DEFAULT);
+            } else {
+                int bpm = Integer.parseInt(textBpm.getText().toString().trim());
+                timer = new Stopwatch(bpm);
+            }
             mHandler.sendEmptyMessage(MSG_START_TIMER);
-        }else
-        if(btnStop == v){
+        } else if (v == buttonStop) {
             mHandler.sendEmptyMessage(MSG_STOP_TIMER);
+        } else if (v == buttonReset) {
+            textBpm.setText("");
+            textCounter.setText("");
+            textStopWatch.setText("");
         }
-    }
+    }//onClick
 }
--- app/src/main/java/app/khash/simplecounter/Stopwatch.java
+++ app/src/main/java/app/khash/simplecounter/Stopwatch.java
@@ -5,18 +5,24 @@ public class Stopwatch {
     private long startTime = 0;
     private long stopTime = 0;
     private boolean running = false;
+    private int bpm;
+    private final float MINUTE = 60.0f;
+
+    public Stopwatch(int bpm) {
+        this.bpm = bpm;
+    }//Stopwatch
 
 
     public void start() {
         this.startTime = System.currentTimeMillis();
         this.running = true;
-    }
+    }//start
 
 
     public void stop() {
         this.stopTime = System.currentTimeMillis();
         this.running = false;
-    }
+    }//stop
 
 
     // elaspsed time in milliseconds
@@ -25,7 +31,7 @@ public class Stopwatch {
             return System.currentTimeMillis() - startTime;
         }
         return stopTime - startTime;
-    }
+    }//getElapsedTime
 
 
     // elaspsed time in seconds
@@ -34,18 +40,33 @@ public class Stopwatch {
             return ((System.currentTimeMillis() - startTime) / 1000);
         }
         return ((stopTime - startTime) / 1000);
-    }
+    }//getElapsedTimeSecs
 
+    //get counter value based on bpm
+    public int getBpm() {
+        if (running) {
+            long elapsedMilli = getElapsedTime();
+            float bpmFloat = elapsedMilli * (bpm / MINUTE);
+            int counter = Math.round(bpmFloat / 1000);
+            return counter;
 
+        } else {
+            return -1;
+        }
+    }//getBpm
 
+    //get the time and bpm as to avoid the difference between the two
+    public String getBpmElapsed() {
+        if (running) {
+            long elapsedMilli = getElapsedTime();
+            float bpmFloat = elapsedMilli * (bpm / MINUTE);
+            int counter = Math.round(bpmFloat / 1000);
+            String output = counter + ";" + elapsedMilli;
+            return output;
+        } else {
+            return null;
+        }
 
-    // sample usage
-    public static void main(String[] args) {
-        Stopwatch s = new Stopwatch();
-        s.start();
-        // code you want to time goes here
-        s.stop();
-        System.out.println("elapsed time in milliseconds: " + s.getElapsedTime());
-    }
+    }//getBpmElapsed
 
 }
