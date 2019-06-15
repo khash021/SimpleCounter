4fad8431e40bba853a6cf3a924fa3c9d31680c47
Khash <khash.021@gmail.com>
2019-06-14 18:45:41 -0700

    Modified class for saving information used for rotating the device

--- app/src/main/java/app/khash/simplecounter/Counter.java
+++ app/src/main/java/app/khash/simplecounter/Counter.java
@@ -1,6 +1,6 @@
 package app.khash.simplecounter;
 
-import android.util.Log;
+import android.os.Bundle;
 
 public class Counter {
 
@@ -15,9 +15,27 @@ public class Counter {
     private int bpm;
     private final float MINUTE = 60.0f;
 
+    //for creating a new mCounter object
     public Counter(int bpm) {
         this.bpm = bpm;
-    }//Stopwatch
+    }//Counter
+
+    public Counter(Bundle savedBundle) {
+
+        //get the info from the bundle
+        this.bpm = savedBundle.getInt(MainActivity.SAVED_BPM);
+        this.startTime = savedBundle.getLong(MainActivity.SAVED_START_TIME);
+        this.pauseTime = savedBundle.getLong(MainActivity.SAVED_PAUSE_TIME);
+        this.pauseDiff = savedBundle.getLong(MainActivity.SAVED_PAUSE_DIFF);
+        this.running = true;
+
+        String state = savedBundle.getString(MainActivity.SAVED_STATE);
+        if (state.equalsIgnoreCase(MainActivity.STATE_RUNNING)) {
+            this.pause = false;
+        } else if (state.equalsIgnoreCase(MainActivity.STATE_PAUSED)) {
+            this.pause = true;
+        }
+    }//Counter
 
 
     public void start() {
@@ -64,8 +82,8 @@ public class Counter {
             float bpmFloat = elapsedMilli * (bpm / MINUTE);
             int counter = (int) bpmFloat/1000;
             String output = counter + ";" + (elapsedMilli /1000);
-            Log.v(TAG, "E-" + elapsedMilli + " : " + "P-" + pauseDiff + " : " +
-                    "BPM-" + bpmFloat + " : " + "C-" + counter);
+//            Log.v(TAG, "E-" + elapsedMilli + " : " + "P-" + pauseDiff + " : " +
+//                    "BPM-" + bpmFloat + " : " + "C-" + counter);
             return output;
         } else {
             return null;
@@ -81,7 +99,7 @@ public class Counter {
         return ((stopTime - startTime) / 1000);
     }//getElapsedSecs
 
-    //get counter value based on bpm
+    //get mCounter value based on bpm
     public int getBpm() {
         if (running) {
             long elapsedMilli = getElapsedMilli();
@@ -94,6 +112,35 @@ public class Counter {
         }
     }//getBpm
 
+    //get all the information regarding the current Counter object, so it could be passed along the
+    //savedInstance Bundle to be used to recreate another identical object
+    //it is passed using the following format   (state;bpm;startTime;pauseTime;pauseDiff)
+    //1 for running state and 0 for paused state
+    public Bundle getSaveBundle() {
+        //create and initialize a new Bundle object
+        Bundle output = new Bundle();
+        if(running) {
+            String state;
+            if (pause) {
+                state = MainActivity.STATE_PAUSED;
+            } else {
+                state = MainActivity.STATE_RUNNING;
+            }
 
+            //put the data in the Bundle
+            output.putString(MainActivity.SAVED_STATE, state);
+            output.putInt(MainActivity.SAVED_BPM, this.bpm);
+            output.putLong(MainActivity.SAVED_START_TIME, this.startTime);
+            output.putLong(MainActivity.SAVED_PAUSE_TIME, this.pauseTime);
+            output.putLong(MainActivity.SAVED_PAUSE_DIFF, this.pauseDiff);
 
-}
+            return output;
+
+        } else {
+            //this is the case that it is not running
+            return null;
+        }
+
+    }//getSavingData
+
+}//Counter Class
