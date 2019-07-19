feb99fc381c83130fca0e58c6ef70534d33470e1
Khash <khash.021@gmail.com>
2019-07-19 10:38:06 -0700

    Created a new class for calculating bpm

---
+++ app/src/main/java/app/khash/simplecounter/BpmCalculator.java
@@ -0,0 +1,9 @@
+package app.khash.simplecounter;
+
+/**
+ * Utility class for calculating the bpm by continuously dividing the counter (provided by the user)
+ * over the total elapsed time.
+ */
+
+public class BpmCalculator {
+}
