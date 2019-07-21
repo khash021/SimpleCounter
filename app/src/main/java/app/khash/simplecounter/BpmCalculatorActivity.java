b43a9e175acc9677d7f9d0104b6cf14232e9782c
Khash <khash.021@gmail.com>
2019-07-21 14:45:21 -0700

    Added new activity for calculating the bpm rate

---
+++ app/src/main/java/app/khash/simplecounter/BpmCalculatorActivity.java
@@ -0,0 +1,22 @@
+package app.khash.simplecounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Class for helping the user find the bpm
+ */
+
+public class BpmCalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }//onCreate
+
+    @Override
+    public void onClick(View v) {
+
+    }//onClick
+}//BpmCalculatorActivity
