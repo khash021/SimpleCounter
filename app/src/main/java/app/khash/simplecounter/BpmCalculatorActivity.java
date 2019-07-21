211b8706bf06ac63348fc88052589f0edec11b48
Khash <khash.021@gmail.com>
2019-07-21 16:27:53 -0700

    Started the code, just declaring variables and OnClickListeners

--- app/src/main/java/app/khash/simplecounter/BpmCalculatorActivity.java
+++ app/src/main/java/app/khash/simplecounter/BpmCalculatorActivity.java
@@ -3,6 +3,8 @@ package app.khash.simplecounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Class for helping the user find the bpm
@@ -10,13 +12,44 @@ import android.view.View;
 
 public class BpmCalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private Button buttonStart, buttonStop, buttonReset, buttonDone, buttonCounterIncrease;
+    private TextView textTimer, textCounter;
+
+    //to keep track of the timer
+    private int counter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bpm_calculator);
+
+        //find views
+        textTimer = findViewById(R.id.text_timer);
+        textCounter = findViewById(R.id.text_counter);
+
+        buttonStart = findViewById(R.id.button_start);
+        buttonStop = findViewById(R.id.button_stop);
+        buttonReset = findViewById(R.id.button_reset);
+        buttonDone = findViewById(R.id.button_done);
+        buttonCounterIncrease = findViewById(R.id.button_counter_increase);
+
+        //set on click listeners on views
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonReset.setOnClickListener(this);
+        buttonDone.setOnClickListener(this);
+        buttonCounterIncrease.setOnClickListener(this);
+
+
     }//onCreate
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
 
     }//onClick
 }//BpmCalculatorActivity
