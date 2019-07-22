e7e12e282821c444cc4eababe9535cc37e2aec00
Khash <khash.021@gmail.com>
2019-07-22 11:38:48 -0700

    Added Handler and some constants

--- app/src/main/java/app/khash/simplecounter/BpmCalculatorActivity.java
+++ app/src/main/java/app/khash/simplecounter/BpmCalculatorActivity.java
@@ -19,6 +19,70 @@ public class BpmCalculatorActivity extends AppCompatActivity implements View.OnC
     //to keep track of the timer
     private int counter;
 
+    //constants to keep track of app state to be used for rotation events
+    private final static int STATE_RESET = 1;
+    private final static int STATE_RUNNING = 2;
+    private final static int STATE_STOPEED = 3;
+    private int appState;
+
+    //constants for the handler
+    private final int MSG_START_TIMER = 0;
+    private final int MSG_STOP_TIMER = 1;
+    private final int MSG_UPDATE_TIMER = 2;
+
+    //Handler refresh rate in ms
+    final int REFRESH_RATE = 100;
+
+    //BPM Calculator class variable
+    BpmCalculator bpmCalculator;
+
+    //the Handler that does the work of refreshing and getting new values from  and updating
+    //the UI
+//    Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//
+//            switch (msg.what) {
+//                //start the timer (Here we start the counter) and start updating
+//                case MSG_START_TIMER:
+//                    counter.start(); //start counter
+//                    handler.sendEmptyMessage(MSG_UPDATE_TIMER);
+//                    break;
+//
+//                //this is where we constantly get the elapsed from counter and update UI
+//                case MSG_UPDATE_TIMER:
+//                    //Get the bpm and elapsed tim from the counter
+//                    String input = counter.getBpmElapsed();
+//                    //make sure it is not null
+//                    if (input == null) {
+//                        handler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER, REFRESH_RATE);
+//                        break;
+//                    }
+//                    //update the UI
+//                    String[] bpmElapsed = input.split(";");
+//                    counterString = bpmElapsed[0];
+//                    elapsedString = bpmElapsed[1];
+//                    textStopWatch.setText(elapsedString);
+//                    textCounter.setText(counterString);
+//                    //update the message every REFRESH_RATE
+//                    handler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER, REFRESH_RATE);
+//                    break;
+//                //Stop the handler
+//                case MSG_STOP_TIMER:
+//                    handler.removeMessages(MSG_UPDATE_TIMER); // no more updates.
+//                    //WTF dummy check in case there is no counter object
+//                    if (counter != null) {
+//                        counter.stop();//stop time
+//                    }
+//                    break;
+//
+//                default:
+//                    break;
+//            }
+//        }
+//    };//handler
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +106,59 @@ public class BpmCalculatorActivity extends AppCompatActivity implements View.OnC
         buttonDone.setOnClickListener(this);
         buttonCounterIncrease.setOnClickListener(this);
 
+        //TODO: figure out the savedInstances
+
 
     }//onCreate
+
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
 
-        }
+        }//switch
 
     }//onClick
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }//onSaveInstanceState
+
+    //Helper method for setting up buttons for reset state
+    private void setupButtonsReset() {
+        buttonStart.setEnabled(true);
+        buttonStop.setEnabled(false);
+        buttonReset.setEnabled(false);
+        buttonCounterIncrease.setEnabled(false);
+        buttonDone.setEnabled(false);
+    }//setupButtonsReset
+
+    //Helper method for setting up buttons for running state
+    private void setupButtonsRunning() {
+        buttonStart.setEnabled(false);
+        buttonStop.setEnabled(true);
+        buttonReset.setEnabled(false);
+        buttonCounterIncrease.setEnabled(true);
+        buttonDone.setEnabled(false);
+    }//setupButtonsRunning
+
+    //Helper method for setting up buttons for stopped state
+    private void setupButtonsStopped() {
+        buttonStart.setEnabled(false);
+        buttonStop.setEnabled(false);
+        buttonReset.setEnabled(true);
+        buttonCounterIncrease.setEnabled(false);
+        buttonDone.setEnabled(true);
+    }//setupButtonsStopped
+
+    //helper method for resetting all the texts
+    private void resetTexts() {
+        textTimer.setText(R.string.zero_decimal);
+        textCounter.setText(R.string.zero);
+    }//resetTexts
+
 }//BpmCalculatorActivity
